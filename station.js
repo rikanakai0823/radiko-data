858e286fed6a75516d4a96876ab5b9ad1461c65a
rikanakai0823 <48682370+rikanakai0823@users.noreply.github.com>
2019-07-30 22:45:27 +0900

    Initial commit

---
+++ station.js
@@ -0,0 +1,8 @@
+let fse = require('fs-extra')
+let rp = require('request-promise')
+let ObjTree = require('objtree')
+let objtree = new ObjTree()
+
+Promise.all(Array.from(Array(47).keys()).map((_e, i) => {
+	return rp(`https://radiko.jp/v3/station/list/JP${i + 1}.xml`);
+})).then(res => res.map((r, i) => fse.writeJson(`station/JP${i + 1}.json`, objtree.parseXML(r))))
