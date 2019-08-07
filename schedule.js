cfe74f2a5f10e49282e4901e4d7ff7cda09213e2
rikanakai0823 <48682370+rikanakai0823@users.noreply.github.com>
2019-08-08 00:54:58 +0900

    update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,19 +1,33 @@
+if (process.argv.length < 3) {
+	console.log('Usage: node schedule.js [mode] [params]');
+	process.exit(-1)
+}
+
 const Promise = require('bluebird')
 const fse = require('fs-extra')
 const rp = require('request-promise')
 const ObjTree = require('objtree')
+const moment = require('moment')
 process.env.TZ = 'Asia/Tokyo'
 let objtree = new ObjTree()
 
-let stime = new Date()
+let abs_mode = (process.argv[2] !== 'd')
+
+let stime = moment()
+if (!abs_mode) {
+	stime = stime.add(parseInt(process.argv[3]), 'd')
+}
+stime = stime.toDate()
 let i_m = `0${stime.getMonth() + 1}`.slice(-2)
 let i_d = `0${stime.getDate()}`.slice(-2)
 let i_y = stime.getFullYear()
 
-let d_y = process.argv[2] || i_y
-let d_m = process.argv[3] || i_m
-let d_d = process.argv[4] || i_d
+let d_y = abs_mode ? process.argv[3] : i_y
+let d_m = abs_mode ? process.argv[4] : i_m
+let d_d = abs_mode ? process.argv[5] : i_d
 let d_ymd = `${d_y}${d_m}${d_d}`
+
+console.log(d_ymd)
 
 Promise.map(Array.from(Array(47).keys()), i => fse.readJson(`./station/JP${i + 1}.json`))
 .then(areas => areas.map(e => e.stations.station.map(s => s.id)))
