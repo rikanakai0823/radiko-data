b100eb2fe584b992fac21c455583445420b8cd90
rikanakai0823 <48682370+rikanakai0823@users.noreply.github.com>
2019-08-08 01:15:24 +0900

    pretty print station json

--- station.js
+++ station.js
@@ -5,4 +5,4 @@ let objtree = new ObjTree()
 
 Promise.all(Array.from(Array(47).keys()).map((_e, i) => {
 	return rp(`https://radiko.jp/v3/station/list/JP${i + 1}.xml`);
-})).then(res => res.map((r, i) => fse.writeJson(`station/JP${i + 1}.json`, objtree.parseXML(r))))
+})).then(res => res.map((r, i) => fse.writeJson(`station/JP${i + 1}.json`, objtree.parseXML(r), {spaces: 2})))
